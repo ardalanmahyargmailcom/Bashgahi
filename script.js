b80da657cd113eb7fb98d2b59d8eb1e9b2fd576a
ardalanmahyargmailcom <ardalanmahyar7@gmail.com>
2025-03-13 01:36:06 +0330

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const results = document.querySelectorAll('.result');
 const saveButton = document.getElementById('save');
 const message = document.getElementById('message');
+const teamRows = document.querySelectorAll('#points-table tbody tr');
 
 // بارگیری داده‌ها از LocalStorage
 const savedData = JSON.parse(localStorage.getItem('matchData')) || {};
@@ -31,41 +32,11 @@ function calculateResults(input) {
     const goalsScoredTeam2Cell = row.querySelector('.goals-scored-team2');
     const goalsConcededTeam1Cell = row.querySelector('.goals-conceded-team1');
     const goalsConcededTeam2Cell = row.querySelector('.goals-conceded-team2');
-    const pointsTeam1Cell = row.querySelector('.points-team1');
-    const pointsTeam2Cell = row.querySelector('.points-team2');
     const result = input.value.split('-');
-    const team1Goals = parseInt(result[0]) || 0; // مقدار پیش فرض 0 در صورت عدم وجود عدد
-    const team2Goals = parseInt(result[1]) || 0; // مقدار پیش فرض 0 در صورت عدم وجود عدد
+    const team1Goals = parseInt(result[0]) || 0;
+    const team2Goals = parseInt(result[1]) || 0;
     const team1Name = input.dataset.team1;
     const team2Name = input.dataset.team2;
 
     if (team1Goals > team2Goals) {
         winsCell.textContent = 1;
-        lossesCell.textContent = 0;
-        pointsTeam1Cell.textContent = 3;
-        pointsTeam2Cell.textContent = 0;
-    } else if (team1Goals < team2Goals) {
-        winsCell.textContent = 0;
-        lossesCell.textContent = 1;
-        pointsTeam1Cell.textContent = 0;
-        pointsTeam2Cell.textContent = 3;
-    } else {
-        winsCell.textContent = 0;
-        lossesCell.textContent = 0;
-        pointsTeam1Cell.textContent = 1;
-        pointsTeam2Cell.textContent = 1;
-    }
-
-    goalsScoredTeam1Cell.textContent = team1Goals;
-    goalsScoredTeam2Cell.textContent = team2Goals;
-    goalsConcededTeam1Cell.textContent = team2Goals;
-    goalsConcededTeam2Cell.textContent = team1Goals;
-
-    // بروزرسانی داده‌های ذخیره شده
-    const key = `${team1Name}-${team2Name}`;
-    savedData[key] = input.value;
-}
-
-// ذخیره داده‌ها در LocalStorage
-saveButton.addEventListener('click', () => {
-    localStorage.setItem('matchData
